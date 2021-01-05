c1d82d22713f46086cce9430fa8e0a4e059d7413
DAVID CHEN <dche242@emory.edu>
2021-01-05 15:10:27 -0500

    clean up imports

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, AsyncStorage } from 'react-native';
 
 import Login from './components/Login';
 import Feed from './components/Feed';
